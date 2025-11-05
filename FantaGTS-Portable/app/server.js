784c1cba84310c4a5484dfd9afef6461d2c080a9
Buselli <lorenzo.buselli@fincantieri.it>
2025-11-05 14:49:21 +0100

    retray

--- FantaGTS-Portable/app/server.js
+++ FantaGTS-Portable/app/server.js
@@ -2494,7 +2494,7 @@ app.get('/api/squadra-partecipante/:partecipanteId', async (req, res) => {
                 a.slot_id,
                 a.offerta,
                 a.costo_finale,
-                s.giocatore_attuale as giocatore,
+                s.giocatore_attuale,
                 s.colore as colore_squadra,
                 s.squadra_numero as numero_squadra,
                 0 as punti_totali
